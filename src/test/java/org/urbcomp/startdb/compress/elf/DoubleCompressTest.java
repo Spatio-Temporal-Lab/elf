397c05d648fcd1ed52091c7bf30b58410bd0d719
Leo <liruiyuanwhu@qq.com>
2022-11-19 09:35:53 +0800

    修改endsign为doublenan

--- src/test/java/org/urbcomp/startdb/compress/elf/DoubleCompressTest.java
+++ src/test/java/org/urbcomp/startdb/compress/elf/DoubleCompressTest.java
@@ -51,6 +51,7 @@ class DoubleCompressTest {
                             new ElfOnChimpCompressor(),
                             new ChimpNCompressor(128),
                             new ElfOnChimpNCompressor(128),
+                            new ElfOnChimpNCompressorO(128),
                             new ElfCompressor(2),
             };
 
@@ -76,6 +77,7 @@ class DoubleCompressTest {
                                 new ElfOnChimpDecompressor(result),
                                 new ChimpNDecompressor(result,128),
                                 new ElfOnChimpNDecompressor(result,128),
+                                new ElfOnChimpNDecompressorO(result,128),
                                 new ElfDecompressor(result, 2)
                 };
                 IDecompressor decompressor = decompressors[i];
