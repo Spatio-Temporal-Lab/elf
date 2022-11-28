aa92118696b75ff6621a8757dd53bea518fc6436
Leo <liruiyuanwhu@qq.com>
2022-11-28 20:13:32 +0800

    优化leading 编码

--- src/main/java/org/urbcomp/startdb/compress/elf/xorcompressor/ElfXORCompressor32.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/xorcompressor/ElfXORCompressor32.java
@@ -11,26 +11,19 @@ public class ElfXORCompressor32 {
     private int size;
     private final static int END_SIGN = Float.floatToIntBits(Float.NaN);
 
-    public final static short[] leadingRepresentation = {0, 0, 0, 0, 0, 0, 0, 0,
-            1, 1, 1, 1, 2, 2, 2, 2,
-            3, 3, 4, 4, 5, 5, 6, 6,
-            7, 7, 7, 7, 7, 7, 7, 7,
-            7, 7, 7, 7, 7, 7, 7, 7,
-            7, 7, 7, 7, 7, 7, 7, 7,
-            7, 7, 7, 7, 7, 7, 7, 7,
+    public final static short[] leadingRepresentation = {
+            0, 0, 0, 0, 0, 0, 1, 1,
+            2, 2, 3, 3, 4, 4, 4, 4,
+            5, 5, 6, 6, 7, 7, 7, 7,
             7, 7, 7, 7, 7, 7, 7, 7
     };
 
-    public final static short[] leadingRound = {0, 0, 0, 0, 0, 0, 0, 0,
-            8, 8, 8, 8, 12, 12, 12, 12,
-            16, 16, 18, 18, 20, 20, 22, 22,
-            24, 24, 24, 24, 24, 24, 24, 24,
-            24, 24, 24, 24, 24, 24, 24, 24,
-            24, 24, 24, 24, 24, 24, 24, 24,
-            24, 24, 24, 24, 24, 24, 24, 24,
-            24, 24, 24, 24, 24, 24, 24, 24
+    public final static short[] leadingRound = {
+            0, 0, 0, 0, 0, 0, 6, 6,
+            8, 8, 10, 10, 12, 12, 12, 12,
+            16, 16, 18, 18, 20, 20, 20, 20,
+            20, 20, 20, 20, 20, 20, 20, 20
     };
-    //    public final static short FIRST_DELTA_BITS = 27;
 
     private final OutputBitStream out;
 
--- src/main/java/org/urbcomp/startdb/compress/elf/xordecompressor/ElfXORDecompressor32.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/xordecompressor/ElfXORDecompressor32.java
@@ -17,7 +17,7 @@ public class ElfXORDecompressor32 {
 
     private final static int END_SIGN = Float.floatToIntBits(Float.NaN);
 
-    private final static short[] leadingRepresentation = {0, 8, 12, 16, 18, 20, 22, 24};
+    private final static short[] leadingRepresentation = {0, 6, 8, 10, 12, 16, 18, 20};
 
     public ElfXORDecompressor32(byte[] bs) {
         in = new InputBitStream(bs);
