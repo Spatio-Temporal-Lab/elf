245607ce99a8425bfd97d89667026b6a8e80561a
Leo <liruiyuanwhu@qq.com>
2023-03-05 16:53:52 +0800

    optimize float compressor

--- src/main/java/org/urbcomp/startdb/compress/elf/compressor32/ElfOnChimpCompressor32.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/compressor32/ElfOnChimpCompressor32.java
@@ -10,34 +10,29 @@ public class ElfOnChimpCompressor32 extends AbstractElfCompressor32 {
         chimp32 = new Chimp32();
     }
 
-    @Override
-    protected int writeInt(int n, int len) {
+    @Override protected int writeInt(int n, int len) {
         OutputBitStream os = chimp32.getOutputStream();
         os.writeInt(n, len);
         return len;
     }
 
-    @Override
-    protected int writeBit(boolean bit) {
+    @Override protected int writeBit(boolean bit) {
         OutputBitStream os = chimp32.getOutputStream();
         os.writeBit(bit);
         return 1;
     }
 
-    @Override
-    protected int xorCompress(int vInt) {
+    @Override protected int xorCompress(int vInt) {
         return chimp32.addValue(vInt);
     }
 
-    @Override
-    public byte[] getBytes() {
+    @Override public byte[] getBytes() {
         return chimp32.getOut();
     }
 
-    @Override
-    public void close() {
+    @Override public void close() {
         // we write one more bit here, for marking an end of the stream.
-        writeBit(false);
+        writeInt(2,2);  // case 10
         chimp32.close();
     }
 }
--- src/main/java/org/urbcomp/startdb/compress/elf/decompressor32/AbstractElfDecompressor32.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/decompressor32/AbstractElfDecompressor32.java
@@ -1,19 +1,12 @@
 package org.urbcomp.startdb.compress.elf.decompressor32;
 
+import org.urbcomp.startdb.compress.elf.utils.Elf32Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractElfDecompressor32 implements IDecompressor32 {
-
-    private final static float[] map10iN =
-            new float[] {1.0f, 1.0E-1f, 1.0E-2f, 1.0E-3f, 1.0E-4f, 1.0E-5f, 1.0E-6f, 1.0E-7f,
-                    1.0E-8f, 1.0E-9f, 1.0E-10f, 1.0E-11f, 1.0E-12f, 1.0E-13f, 1.0E-14f,
-                    1.0E-15f, 1.0E-16f, 1.0E-17f, 1.0E-18f, 1.0E-19f, 1.0E-20f};
-    private final static float[] map10iP =
-            new float[] {1.0f, 1.0E1f, 1.0E2f, 1.0E3f, 1.0E4f, 1.0E5f, 1.0E6f, 1.0E7f,
-                    1.0E8f, 1.0E9f, 1.0E10f, 1.0E11f, 1.0E12f, 1.0E13f, 1.0E14f,
-                    1.0E15f, 1.0E16f, 1.0E17f, 1.0E18f, 1.0E19f, 1.0E20f};
+    private int lastBetaStar = Integer.MAX_VALUE;
 
     public List<Float> decompress() {
         List<Float> values = new ArrayList<>(1024);
@@ -25,24 +18,31 @@ public abstract class AbstractElfDecompressor32 implements IDecompressor32 {
     }
 
     private Float nextValue() {
-        int flag = readInt(1);
-
         Float v;
-        if (flag == 0) {
-            v = xorDecompress();
+
+        if (readInt(1) == 0) {
+            v = recoverVByBetaStar();               // case 0
+        } else if (readInt(1) == 0) {
+            v = xorDecompress();                    // case 10
         } else {
-            int betaStar = readInt(3);
-            Float vPrime = xorDecompress();
-            int sp = (int) Math.floor(Math.log10(Math.abs(vPrime)));
-            if (betaStar == 0) {
-                v = get10iN(-sp - 1);
-                if (vPrime < 0) {
-                    v = -v;
-                }
-            } else {
-                int alpha = betaStar - sp - 1;
-                v = roundUp(vPrime, alpha);
+            lastBetaStar = readInt(3);          // case 11
+            v = recoverVByBetaStar();
+        }
+        return v;
+    }
+
+    private float recoverVByBetaStar() {
+        float v;
+        Float vPrime = xorDecompress();
+        int sp = (int) Math.floor(Math.log10(Math.abs(vPrime)));
+        if (lastBetaStar == 0) {
+            v = Elf32Utils.get10iN(-sp - 1);
+            if (vPrime < 0) {
+                v = -v;
             }
+        } else {
+            int alpha = lastBetaStar - sp - 1;
+            v = Elf32Utils.roundUp(vPrime, alpha);
         }
         return v;
     }
@@ -51,34 +51,4 @@ public abstract class AbstractElfDecompressor32 implements IDecompressor32 {
 
     protected abstract int readInt(int len);
 
-    private static float get10iN(int i) {
-        if (i <= 0) {
-            throw new IllegalArgumentException("The argument should be greater than 0");
-        }
-        if (i >= map10iN.length) {
-            return Float.parseFloat("1.0E-" + i);
-        } else {
-            return map10iN[i];
-        }
-    }
-
-    private static float get10iP(int i) {
-        if (i <= 0) {
-            throw new IllegalArgumentException("The argument should be greater than 0");
-        }
-        if (i >= map10iP.length) {
-            return Float.parseFloat("1.0E" + i);
-        } else {
-            return map10iP[i];
-        }
-    }
-
-    private static float roundUp(float v, int alpha) {
-        float scale = get10iP(alpha);
-        if (v < 0) {
-            return (float) (Math.floor(v * scale) / scale);
-        } else {
-            return (float) (Math.ceil(v * scale) / scale);
-        }
-    }
 }
