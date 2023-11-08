991e189e71fc59c027f07786ab48ef5815c8f5ef
LIIIz <1519316637@qq.com>
2023-11-08 20:31:27 +0800

    在改 Buff

--- src/main/java/com/github/Tranway/buff/SparseResult.java
+++ src/main/java/com/github/Tranway/buff/SparseResult.java
@@ -1,15 +1,57 @@
 package com.github.Tranway.buff;
 
 import java.util.ArrayList;
+
 public class SparseResult {
     boolean flag;
     byte frequent_value;
     byte[] bitmap;
     boolean[] isFrequentValue;
     ArrayList<Byte> outliers;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public byte getFrequent_value() {
+        return frequent_value;
+    }
+
+    public void setFrequent_value(int frequent_value) {
+        this.frequent_value = (byte) frequent_value;
+    }
+
+    public byte[] getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(byte[] bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public boolean[] getIsFrequentValue() {
+        return isFrequentValue;
+    }
+
+    public void setIsFrequentValue(boolean[] isFrequentValue) {
+        this.isFrequentValue = isFrequentValue;
+    }
+
+    public ArrayList<Byte> getOutliers() {
+        return outliers;
+    }
+
+    public void setOutliers(ArrayList<Byte> outliers) {
+        this.outliers = outliers;
+    }
+
     SparseResult(int batch_size) {
         flag = false;
-        bitmap = new byte[batch_size / 8];
+        bitmap = new byte[batch_size / 8 + 1];
         outliers = new ArrayList<>();
         isFrequentValue = new boolean[batch_size];
     }
--- src/main/java/org/urbcomp/startdb/compress/elf/Main.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/Main.java
@@ -1,36 +1,31 @@
 package org.urbcomp.startdb.compress.elf;
 
+import com.github.Tranway.buff.BuffCompressor;
+import com.github.Tranway.buff.BuffDecompressor;
 import org.urbcomp.startdb.compress.elf.compressor.*;
 import org.urbcomp.startdb.compress.elf.decompressor.*;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        double[] vs = new double[] {
-                        93.85,
-                        -38.88,
-                        532.64,
-                        326.52,
-                        -1107.21,
-                        211.1,
-                        9.34,
-                        238.77,
-                        -103.54};
-        ICompressor compressor = new ElfOnChimpPlusCompressor();
-        for (double v : vs) {
-            compressor.addValue(v);
-        }
-        compressor.close();
-
-        System.out.println(compressor.getSize());
-
-        byte[] result = compressor.getBytes();
-        IDecompressor decompressor = new ElfOnChimpPlusDecompressor(result);
-        List<Double> values = decompressor.decompress();
-        assert(values.size() == vs.length);
-        for (int i = 0; i < values.size(); i++) {
-            assert(vs[i] == values.get(i));
-        }
+    public static void main(String[] args) throws IOException {
+        double[] vs = new double[]{
+                93.85,
+                38.88,
+                -532.64,
+                326.52,
+                1107.21,
+                -211.1,
+                9.34,
+                -238.77,
+                103.549888
+        };
+        BuffCompressor compressor = new BuffCompressor();
+        compressor.compress(vs);
+        byte[] result = compressor.getOut();
+        BuffDecompressor decompressor = new BuffDecompressor(result);
+        System.out.println(Arrays.toString(decompressor.decompress()));
     }
 }
